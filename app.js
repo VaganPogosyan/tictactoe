9cd2661b66f70ee1ae95a4f8ab6bd083a8ba5549
Vagan Pogosyan <vagan2502@gmail.com>
2021-04-29 02:06:27 -0400

    creating winning logic

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 $(() => {
 
     for (let i = 1; i <= 9; i++) {
-        const $square = $('<div> ' + i + ' </div>');
-        $($square).appendTo('#field').addClass('square');
+        const $square = $('<div></div>');
+        $($square).appendTo('#field').addClass('square').attr('id', '#square' + i);
     }
 
+    let $count = true;
 
+    $('.square').one('click', function clicking() {
+        if ($count === false) {
+            $('<p>X</p>').appendTo(this).addClass('sign');
+            $count = !$count;
+        } else {
+            $('<p>O</p>').appendTo(this).addClass('sign');
+            $count = !$count;
+        }
 
-    const $winningCombinations = [
-        [0, 0, 0],
-        [0, 0, 0]
-    ]
+    })
+
+    // const $combinations = [
+    //     [1, 2, 3],
+    //     [4, 5, 6],
+    //     [7, 8, 9]
+    // ]
+
+    // const $winningCombinations = [
+    //     [1, 2, 3],
+    //     [4, 5, 6],
+    //     [7, 8, 9],
+    //     [1, 4, 7],
+    //     [2, 5, 8],
+    //     [3, 6, 9],
+    //     [1, 5, 9],
+    //     [3, 5, 7]
+    // ];
+
+    // game logic
+    // if ($('.sign') ===  )
+
 })
 
